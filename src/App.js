789edc8b760b43b5be17b215533d485916b2956b
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2022-09-20 13:42:01 +0200

    final-submission

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './Profile/Profile';
-const handleName=(FullName)=>{
-  alert(`Hi,I'm ${FullName}`)
-}
+import Image from './Profile/image.jpg'
+
+
 function App() {
+  const handleName=(FullName)=>{
+    alert(`Hi,I'm ${FullName}`);
+  }
+const Parameters={ FullName:'Mohamed Amine Ben Hmida', Bio :'I ❤️ programming. I’ve been doing this for a decade as a full stack developer and as a backend engineer.My dream job is at a startup company where I can work on the background, API-driven apps or service-oriented architecture with Node.', Profession :'Software Engineer'}
   return (
+  
     <div className='Parent'>
-      <Profile FullName='Mohamed Amine Ben Hmida' Bio ='I ❤️ programming. I’ve been doing this for a decade as a full stack developer and as a backend engineer. My dream job is at a startup company where I can work on the background, API-driven apps or service-oriented architecture with Node.' Profession ='Software Engineer'></Profile>
+      <Profile parameters={Parameters} handleName={handleName}>
+        <img id="first"src={Image} alt=""/>
+        </Profile>
       
-handleName={handleName()}
-<image/>
-
-
     </div>
 
 
   );
 
-}
-
-
+};
 export default App;
 
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,14 +1,26 @@
 import React from "react";
-import image from '../image.jpg'
-const Profile =(props) => {
+import PropTypes from "prop-types";
+const Profile =({parameters,handleName,children}) => {
 
     return (
 <>
-<h1> {props.FullName}</h1>
-<p> {props.Bio}</p>
-<h2> {props.Profession}</h2>
-<img src={image} alt=""/>
+<h1> {parameters.FullName}</h1>
+<p> {parameters.Bio}</p>
+<h2> {parameters.Profession}</h2>
+{children}
+<br />
+      <button id="click" onClick={() => handleName(parameters.FullName)}>Click me</button>
+      <br />
 </>
     )
 }
+Profile.propTypes = {
+    FullName: PropTypes.string.isRequired,
+    Bio: PropTypes.string,
+    Profession: PropTypes.string,
+    handleAlert: PropTypes.func,
+  };
+  Profile.defaultProps = {
+    Bio: "To be defined",
+  };
   export default Profile
